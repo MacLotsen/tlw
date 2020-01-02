0286bb3c98472d5f8ab57daee956955538a286aa
enijenhuis <erik.nijenhuis@altran.com>
2020-01-02 09:40:05 +0100

    Add license

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -1,9 +1,34 @@
-//
-// Created by enijenhuis on 30-12-2019.
-//
+/*
+ * Typed Lua Wrapping
+ * Copyright (C) 2019  Erik Nijenhuis
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #include "benchmark.h"
 #include "classic.h"
 #include "impl.h"
+
+#define VERSION_TO_STRING(x) #x
+#define VERSION_STR(x) VERSION_TO_STRING(x)
+
+static const char * disclaimer = "TLW version " VERSION_STR(TLW_VERSION) ", Copyright (C) 2019 Erik Nijenhuis\n"
+                                 "TLW comes with ABSOLUTELY NO WARRANTY. This is free\n"
+                                 "software, and you are welcome to redistribute it un-\n"
+                                 "der certain conditions.\n";
+
 
 std::vector<Benchmark> benchmarks = {
         {"Property Get",         "local num = example.number",      new ClassicPropertyBenchmark,            new ImplPropertyBenchmark},
@@ -22,6 +47,7 @@ std::vector<Benchmark> benchmarks = {
 };
 
 int main(int argc, char **argv) {
+    std::cout << disclaimer << std::endl;
     for (auto b : benchmarks) {
         b.run();
     }

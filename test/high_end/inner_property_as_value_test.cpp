e5eea41b8ee0cfefb733957075b73b66f000737c
Erik Nijenhuis <erik@frisionum.com>
2020-04-24 10:03:48 +0200

    Add non-working property testing

---
+++ test/high_end/inner_property_as_value_test.cpp
@@ -0,0 +1,30 @@
+/*
+ * Typed Lua Wrapping
+ * Copyright (C) 2019  Erik Nijenhuis
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "high_end_user_test.h"
+
+
+
+TEST_F(high_end_user_test, test_inner_property_as_value_test) {
+    lua.src("e1 = entity() e1.position.x = 5")();
+
+    tlw::entity *e1 = lua["e1"];
+
+    ASSERT_EQ(5, e1->position.x);
+}
